7015d67243a9391a7609db1bba843cd79495bf66
Aeryle <47262222+Aeryle@users.noreply.github.com>
2021-07-21 22:14:48 +0200

    Fine-tune ESLint and add Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,20 @@
 module.exports = {
-    env: {
-        jest: true,
-    },
-    extends: 'airbnb-base',
-    rules: {
-        'comma-dangle': 0,
-        'no-underscore-dangle': 0,
-        'no-param-reassign': 0,
-        'no-return-assign': 0,
-        camelcase: 0,
-    }
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+
+  rules: {
+    "no-unused-vars": "warn",
+    "import/no-extraneous-dependencies": [
+      "error",
+      { devDependencies: ["**/*.test.js", "**/*.spec.js", "**/seed.js"] },
+    ],
+  },
 };
